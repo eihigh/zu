cd68b9b4f46b1d6bae729e2db76c28743ac085a0
eihigh <eihigh.contact@gmail.com>
2020-02-06 19:37:25 +0900

    update

--- draw.go
+++ draw.go
@@ -49,26 +49,3 @@ func Copy(src, dst *ebiten.Image, opts ...DrawOptionFn) {
 	}
 	dst.DrawImage(src, dio)
 }
-
-// var (
-// 	opt = &ebiten.DrawImageOptions{}
-// )
-//
-// type DrawOptionFn func()
-//
-// func DrawImage(src, dst *ebiten.Image, fns ...DrawOptionFn) {
-// 	opt.GeoM.Reset()
-// 	for _, fn := range fns {
-// 		fn()
-// 	}
-// 	if dst == nil {
-// 		dst = screen
-// 	}
-// 	dst.DrawImage(src, opt)
-// }
-//
-// func Translate(x, y float64) DrawOptionFn {
-// 	return func() {
-// 		opt.GeoM.Translate(x, y)
-// 	}
-// }
